e4fb2f02940a81f34aa1f6e4895b0266a0c97246
Ngô Hữu Nam <161062684+nhn202204@users.noreply.github.com>
2024-02-28 06:43:50 +0700

    fix

--- app/[Locale]/characters/page.tsx
+++ app/[Locale]/characters/page.tsx
@@ -4,11 +4,11 @@ import CharacterSelector from '@/components/CharacterSelector';
 import { CharacterDatas } from '@/data/type';
 
 import { promises as fs } from 'fs';
-import path from 'path';
+// import path from 'path';
 
 export default async function CharacterPage() {
 
-  const fileCharacters = await fs.readFile(path.resolve() + '/data/characters.json', 'utf8');
+  const fileCharacters = await fs.readFile(process.cwd() + '/data/characters.json', 'utf8');
   const datas: CharacterDatas = JSON.parse(fileCharacters)
 
   return (
